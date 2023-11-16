f618f4b26e43ec9c3c42e5da29be8a06da47da42
MsLois <loisboafowaa@gmail.com>
2023-11-15 16:27:32 -0800

    comparisms

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,8 +2,8 @@
 
 /**
  * wildcmp - Compares two strings
- * @s1: Pointer to string one
- * @s2: Pointer to string two
+ * @s1: Pointer to string params
+ * @s2: Pointer to string params
  * Return: 0
  */
 
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if (*s2 != '\0' && *s2 == 's')
+		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
